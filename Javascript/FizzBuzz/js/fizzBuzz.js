cc890bba60751c44e45bde99bbb4c80c45f64f1c
Jared Avila <jared.c.avila@gmail.com>
2018-10-27 07:15:38 -0700

    update

--- Javascript/FizzBuzz/js/fizzBuzz.js
+++ Javascript/FizzBuzz/js/fizzBuzz.js
@@ -1,13 +1,22 @@
-function fizzBuzz(n){
+function fizzBuzz(n) {
     var arr = [];
-    for (i = 1; i <= n; i++){
-        if (i%3 === 0 && i%5 === 0){
+    var num = parseInt(n);
+    if(Number.isInteger(num) == false){
+        alert("Only positive numbers please.");
+        return;
+    }
+    if (num < 0) {
+        alert("Only positive numbers please.");
+        return;
+    }
+    for (i = 1; i <= num; i++) {
+        if (i % 3 === 0 && i % 5 === 0) {
             arr.push("FizzBuzz");
-        } else if(i%3 === 0){
+        } else if (i % 3 === 0) {
             arr.push("Fizz");
-        } else if(i%5 === 0){
+        } else if (i % 5 === 0) {
             arr.push("Buzz");
-        } else{
+        } else {
             arr.push(i);
         }
     }
